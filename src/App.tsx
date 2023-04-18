6223e60d4a67cf6e686d900a15b615c4c6afb038
ayakuma309 <ayako.30901@gmail.com>
2023-04-18 19:30:35 +0900

    ✨ Appコンポーネント(ログイン処理)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import styles from "./App.module.css";
+import Feed from './components/Feed';
+import Auth from './components/Auth';
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        dispatch(
+          login({
+            uid: authUser.uid,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  },[dispatch]);
+  return(
+    <>
+      {user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  )
 }
 
 export default App;
