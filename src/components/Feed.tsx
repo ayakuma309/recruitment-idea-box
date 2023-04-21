7d71e42c74bee8f088109ec46088d50a818b150c
ayakuma309 <ayako.30901@gmail.com>
2023-04-21 11:38:03 +0900

    💄 add: floatボタンを追加

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import AfterLoginHeader from './AfterLoginHeader'
-
-import styles from "./styles/Feed.module.css";
+import styles from './styles/Feed.module.css'
 import InputPost from './InputPost';
 const Feed = () => {
   return (
     <div>
       <AfterLoginHeader />
       <InputPost />
+      <div className={styles.post_float_btn}>
+        <p className={styles.post_plus}>投稿</p>
+      </div>
     </div>
   )
 }
