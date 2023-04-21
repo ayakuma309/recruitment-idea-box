0d40cc6909005c391fea4bb1b8276dacda266a86
ayakuma309 <ayako.30901@gmail.com>
2023-04-21 15:07:45 +0900

    ✨ add: コメント機能

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./styles/Post.module.css";
+
+import { db } from '../firebase';
+import firebase from "firebase/app";
+
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
 import { makeStyles } from '@material-ui/core/styles';
 import {
@@ -9,8 +15,9 @@ import {
   Button,
   Typography
 } from '@material-ui/core';
+import SendIcon from "@material-ui/icons/Send";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 275,
     width: '48%',
@@ -25,8 +32,13 @@ const useStyles = makeStyles({
   buttons: {
     display: 'flex',
     justifyContent: 'space-between',
-  }
-});
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
 
 
 interface PROPS {
@@ -37,8 +49,66 @@ interface PROPS {
   username: string;
 }
 
+interface COMMENT {
+  id: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
+
 const Post: React.FC<PROPS> = (props) => {
   const classes = useStyles();
+  const user = useSelector(selectUser);
+
+  //コメントする際のuseState
+  const [openComments, setOpenComments] = useState(false);
+  //コメント投稿フォームのuseState
+  const [comment, setComment] = useState("");
+  //コメント一覧のuseState
+  const [comments, setComments] = useState<COMMENT[]>([
+    {
+      id: "",
+      text: "",
+      username: "",
+      timestamp: null,
+    },
+  ]);
+
+  //コメント一覧取得(postのidに紐づいたコメントを持ってくる)
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .doc(props.postId)
+      .collection("comments")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            text: doc.data().text,
+            username: doc.data().username,
+            timestamp: doc.data().timestamp,
+          }))
+        );
+      });
+
+    return () => {
+      unSub();
+    };
+  }, [props.postId]);
+
+  //コメント新規投稿
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //一つの投稿に紐づいている
+    db.collection("posts").doc(props.postId).collection("comments").add({
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
+
   return (
     <>
       <Card className={classes.root}>
@@ -55,12 +125,51 @@ const Post: React.FC<PROPS> = (props) => {
         <CardActions className={classes.buttons}>
           <div>
             <Button size="small">いいね</Button>
-            <Button size="small">コメント</Button>
+            <Button
+              size="small"
+              onClick={() => setOpenComments(!openComments)}
+            >
+              コメント
+            </Button>
+            {/* コメントするか */}
+            {openComments && (
+              <>
+                {/* コメント一覧 */}
+                {comments.map((com) => (
+                  <div key={com.id} className={styles.post_comment}>
+
+                    <span className={styles.post_commentUser}>{com.username}</span>
+                    <span className={styles.post_commentText}>{com.text} </span>
+                  </div>
+                ))}
+
+                {/* 新規コメント */}
+                <form onSubmit={newComment}>
+                  <div className={styles.post_form}>
+                    <input
+                      className={styles.post_input}
+                      type="text"
+                      placeholder="Type new comment..."
+                      value={comment}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setComment(e.target.value)
+                      }
+                    />
+                    <button
+                      disabled={!comment}
+                      className={
+                        comment ? styles.post_button : styles.post_buttonDisable
+                      }
+                      type="submit"
+                    >
+                      <SendIcon className={styles.post_sendIcon} />
+                    </button>
+                  </div>
+                </form>
+              </>
+            )}
           </div>
-          <div>
-            <Button size="small">編集</Button>
-            <Button size="small">削除</Button>
-          </div>
+        
         </CardActions>
       </Card>
     </>
