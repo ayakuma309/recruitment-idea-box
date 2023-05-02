effda8d46d6f54641319b00e23f5fa7209de07b8
ayakuma309 <ayako.30901@gmail.com>
2023-05-02 14:23:35 +0900

    ✨ add: google認証

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
@@ -17,4 +16,4 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const db = firebaseApp.firestore();
 export const auth = firebase.auth();
-export const storage = firebase.storage();
+export const provider = new firebase.auth.GoogleAuthProvider();
